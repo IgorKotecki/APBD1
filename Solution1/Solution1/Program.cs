421445d8f2c9fd2244ac442c524e1f161b8853d8
s27014 <s27014@pjwstk.edu.pl>
2024-03-18 09:27:21 +0100

    Added CalculateAverage method

--- Solution1/Solution1/Program.cs
+++ Solution1/Solution1/Program.cs
@@ -9,3 +9,10 @@ Console.WriteLine("commit 2");
 Console.WriteLine("commit 3");
 
 Console.WriteLine("commit 3.5");
+
+return ;
+
+static double CalculateAverage(int[] arr)
+{
+    return arr.Average();
+}
